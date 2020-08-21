2c574ab78282a41e945bc0c8b282ec2c52f94527
aterziev <a.terziev1415@gmail.com>
2020-08-21 16:37:32 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/Home/home';
 import Nav from './components/Nav/nav';
+import Login from './components/Login/login';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default () => (
     <Navbar bg="light" expand="lg">
@@ -7,8 +8,8 @@ export default () => (
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Login</Nav.Link>
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/login">Login</Link>
             </Nav>
             <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
